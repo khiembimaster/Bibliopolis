dd192dc57ed41738649276b53e6df75e5179b6e2
21127317 <nhkhiem21@clc.fitus.edu.vn>
2024-04-23 16:14:59 +0700

    configuring database adapter

--- auth.ts
+++ auth.ts
@@ -1,6 +1,18 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+import prisma from "./client"
 
 export const { handlers, auth, signIn, signOut} = NextAuth({
-  providers: [Google],
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    })
+  ],
+  session: {
+    strategy: 'jwt'
+  }
 })
