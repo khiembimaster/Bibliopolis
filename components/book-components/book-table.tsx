64a99992ba51b0a1e262c61795c946d64051cf21
21127317 <nhkhiem21@clc.fitus.edu.vn>
2024-05-06 23:30:33 +0700

    add is approved state for order status

--- components/book-components/book-table.tsx
+++ components/book-components/book-table.tsx
@@ -40,67 +40,88 @@ export async function BookTable() {
 
    
     return (
-        <Table>
-            <TableCaption>A list of books.</TableCaption>
-            <TableHeader >
-                <TableRow>
-                    <TableHead >ID</TableHead>
-                    <TableHead>Title</TableHead>
-                    <TableHead>Author</TableHead>
-                    <TableHead>Genre</TableHead>
-                    <TableHead>Isbn</TableHead>
-                    <TableHead>Price</TableHead>
-                    <TableHead>Publisher</TableHead>
+        <>
 
-                    <TableHead>Publication year</TableHead>
-                    <TableHead>Stock quantity</TableHead>
-                    <TableHead>Rating</TableHead>
-                    <TableHead>Description</TableHead>
-                
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                {books.map((book) => (
-                    <TableRow key={book.id}>
-                        <TableCell id={"id"+String(book.id)} className="font-medium">{book.id}</TableCell>
-                        <TableCell>
-                            <Input id={"inputTitle"+String(book.id)} defaultValue={book.title} />
-                        </TableCell>
-                        <TableCell>
-                            <Input id={"inputAuthor"+String(book.id)} defaultValue={book.author} />
-                        </TableCell>
-                        <TableCell>
-                            <ul>
-                           {book.genres.map((genre) =>(
-                           <li key={genre.id}>{genre.name}</li> 
-                           ))} 
-                           </ul>
-                           <AddGenreButton book={book}/>
-                        </TableCell>
-                        
-                        <TableCell> <Input id={"inputIsbn"+String(book.id)} defaultValue= {book.isbn}/></TableCell>
-                        <TableCell> <Input id={"inputPrice"+String(book.id)} defaultValue= {book.price.toString()}/></TableCell>
-                        <TableCell><Input id={"inputPublisher"+String(book.id)} defaultValue= {book.publisher}/></TableCell>
-                        <TableCell><Input  id={"inputDate"+String(book.id)} type="date" defaultValue= {formatDate(book.publication_year.toLocaleDateString())}/>
-                            
-                        </TableCell>    
-                        <TableCell >
-                        <Input  id={"inputQuantity"+String(book.id)} className="w-full" defaultValue= {String(book.stock_quantity)}/>
-                            
-                            </TableCell>
-                        <TableCell id={"bookRating"+String(book.id)}>{String(book.rating)}</TableCell>
-                   
-                        <TableCell><EditBook book={book}/></TableCell>
+            <Table>
+                <TableCaption>A list of books.</TableCaption>
+                <TableHeader >
+                    <TableRow>
+                        <TableHead >ID</TableHead>
+                        <TableHead>Title</TableHead>
+                        <TableHead>Author</TableHead>
+                        <TableHead>Genre</TableHead>
+                        <TableHead>Isbn</TableHead>
+                        <TableHead>Price</TableHead>
+                        <TableHead>Publisher</TableHead>
 
-                        <TableCell>
-                                <UpdateButton id={book.id} book={book}/>
-                                <Button className="bg-red-500 hover:bg-red-600">✗</Button>
-                        </TableCell>
-                     
+                        <TableHead>Publication year</TableHead>
+                        <TableHead>Stock quantity</TableHead>
+                        <TableHead>Rating</TableHead>
+                        <TableHead>Description</TableHead>
+                    
                     </TableRow>
-                ))}
-            </TableBody>
+                </TableHeader>
+                <TableBody>
+                    {books.map((book) => (
+                        <TableRow key={book.id}>
+                            <TableCell id={"id"+String(book.id)} className="font-medium">{book.id}</TableCell>
+                            <TableCell>
+                                <Input id={"inputTitle"+String(book.id)} defaultValue={book.title} />
+                            </TableCell>
+                            <TableCell>
+                                <Input id={"inputAuthor"+String(book.id)} defaultValue={book.author} />
+                            </TableCell>
+                            <TableCell>
+                                <ul>
+                            {book.genres.map((genre) =>(
+                            <li key={genre.id}>{genre.name}</li> 
+                            ))} 
+                            </ul>
+                            <AddGenreButton book={book}/>
+                            </TableCell>
+                            
+                            <TableCell> <Input id={"inputIsbn"+String(book.id)} defaultValue= {book.isbn}/></TableCell>
+                            <TableCell> <Input id={"inputPrice"+String(book.id)} defaultValue= {book.price.toString()}/></TableCell>
+                            <TableCell><Input id={"inputPublisher"+String(book.id)} defaultValue= {book.publisher}/></TableCell>
+                            <TableCell><Input  id={"inputDate"+String(book.id)} type="date" defaultValue= {formatDate(book.publication_year.toLocaleDateString())}/>
+                                
+                            </TableCell>    
+                            <TableCell >
+                            <Input  id={"inputQuantity"+String(book.id)} className="w-full" defaultValue= {String(book.stock_quantity)}/>
+                                
+                                </TableCell>
+                            <TableCell id={"bookRating"+String(book.id)}>{String(book.rating)}</TableCell>
+                    
+                            <TableCell><EditBook book={book}/></TableCell>
 
-        </Table>
+                            <TableCell>
+                                    <UpdateButton id={book.id} book={book}/>
+                                    <Button className="bg-red-500 hover:bg-red-600">✗</Button>
+                            </TableCell>
+                        
+                        </TableRow>
+                    ))}
+                </TableBody>
+            
+            </Table>
+            <div className="flex items-center justify-end space-x-2 py-4">
+            <Button
+              variant="outline"
+              size="sm"
+            //   onClick={() => table.previousPage()}
+            //   disabled={!table.getCanPreviousPage()}
+            >
+              Previous
+            </Button>
+            <Button
+              variant="outline"
+              size="sm"
+            //   onClick={() => table.nextPage()}
+            //   disabled={!table.getCanNextPage()}
+            >
+              Next
+            </Button>
+          </div>
+        </>
     )
 }
